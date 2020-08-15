e7c0dc257aeb278b2b575671996e355429accf02
61101110286 <61101110286@psu.ac.th>
2020-08-15 22:50:51 +0700

    part 4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import WordCard from './WordCard' ;
+const word = "Hello";
 function App() {
  return (
  <div>
- Hello World
+ {
+    <WordCard value="hello"/>
+ }
  </div>
  );
 }
+
 export default App;
