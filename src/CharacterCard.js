a97a86adf09946e7ff412fc847e0a0d722e99dc2
61101110286 <61101110286@psu.ac.th>
2020-08-15 23:29:00 +0700

    part 6 lodash

--- src/CharacterCard.js
+++ src/CharacterCard.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect , useRef } from 'react';
 export default function CharacterCard(props) {
  const [active, setActive] = useState(false);
+ const attemptRef = useRef(props.attempt); 
  
  const activate = () => {
     if(!active){
@@ -10,6 +11,14 @@ export default function CharacterCard(props) {
     
    
  }
+
+ useEffect(() => {
+    if(attemptRef.current != props.attempt){
+    setActive(false)
+    attemptRef.current = props.attempt
+    }
+    })
+   
  const className = `card ${active ? 'activeCard': ''}`
  return (
  <div className={className} onClick={activate}>{props.value}</div>
