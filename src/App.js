c677532e1c0bdb1ecea070beeb95a59de36a46f1
61101110286 <61101110286@psu.ac.th>
2020-08-16 03:11:55 +0700

    งานท้ายการทดลอง1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React from 'react';
 import './App.css';
 
+
 import WordCard from './WordCard' ;
+const element = <h1>Hello, world</h1>;
 const word = "Hello";
+
+
 function App() {
- return (
- <div>
+   return (
+      <h1 className = "container">Welcome to "mind Guessing"
+ <div className="unactiveCard">
  {
+   
     <WordCard value="17046"/>
  }
+  <h2 className = "buttom">กติกา : ให้ทายตัวเลขที่ปรากฏอยู่บนหน้าจอ หากสามารถทายถูก จะมีคอนโซลแสดงความยินดีโผล่ออกมา ขอให้โชคดี
+  </h2> 
  </div>
+ </h1>
+ 
+ 
+ 
  );
 }
+
 
 export default App;
